ebc691dbc5675a805ce9c73e649623df307b8d24
emanuelklado <emanuelklado@msn.com>
2022-04-01 23:37:04 -0300

    Setup Redux Ok

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,6 @@
-// Coloque aqui suas actions
+export const LOGIN = 'LOGIN';
+
+export const actionLogin = (payload) => ({
+  type: LOGIN,
+  payload,
+});
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,16 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  currencies: [],
+  expenses: [],
+};
+
+const wallet = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  case 'ACTION': // NÃO ESQUECER DE ALTERAR ACTION
+    return state;
+  default:
+    return state;
+  }
+};
+
+export default wallet;
